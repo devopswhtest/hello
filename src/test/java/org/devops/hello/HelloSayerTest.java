3e4d5536411f6e2edafa44f571c763741931f58a
devopswh <devopswhtest@noname.org>
2015-06-30 09:35:24 +0200

    testcases for main

--- src/test/java/org/devops/hello/HelloSayerTest.java
+++ src/test/java/org/devops/hello/HelloSayerTest.java
@@ -2,9 +2,11 @@ package org.devops.hello;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+
 public class HelloSayerTest extends TestCase {
 
-	
+	@Test
 	public void testSayHello() {
 		String hello = new HelloSayer().sayHello("Mama");
 		assertEquals("Hello Mama", hello);
---
+++ src/test/java/org/devops/hello/TestMe.java
@@ -0,0 +1,16 @@
+package org.devops.hello;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TestMe {
+
+	@Test
+	public void aComplexComputation() {
+		String a = "a";
+		String b = "a";
+		Assert.assertEquals(a, b);
+		
+	}
+}
