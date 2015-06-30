e9ba5d6b1d0c0fcba40d8a6f3543a18500707408
devopswh <devopswhtest@noname.org>
2015-06-30 09:21:11 +0200

    Huettermann Main

---
+++ src/main/java/org/devops/hello/Main.java
@@ -0,0 +1,32 @@
+package org.devops.hello;
+
+
+/**
+ * Created by michaelhuettermann  
+ */
+public class Main {
+
+    private Main() {}
+    
+    public static void main ( String[] args ) {
+        System.out.println("hallo");
+        System.out.println("hallo");
+        System.out.println("hallo");
+        
+        int a = 0;
+        int b = 0;
+        
+        for (int i = 0; i < 10; i++) {  
+           // ...
+        }
+        
+        int c = 0;
+        int d = 0;
+        
+        for (int i = 0; i < 10; i++) {  
+           // ...
+        }
+        
+    }
+
+}
